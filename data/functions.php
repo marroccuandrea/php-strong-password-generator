59f3d70390c10da0d9497f183b2620a18b8555b0
marroccuandrea <marroccuandrea@yahoo.it>
2024-04-16 16:18:48 +0200

    fix warning variabile non definita

--- data/functions.php
+++ data/functions.php
@@ -2,13 +2,15 @@
 
 function randomPassword()
 {
-    $number = $_GET["numero"];
-    $simboli = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!?&%$<>^+-*/()[]{}@#_=";
-    $pass = array();
-    $simboli_length = strlen($simboli) - 1;
-    for ($i = 0; $i < $number; $i++) {
-        $n = rand(0, $simboli_length);
-        $pass[] = $simboli[$n];
+    if (isset($_GET["numero"])) {
+        $number = $_GET["numero"];
+        $simboli = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!?&%$<>^+-*/()[]{}@#_=";
+        $pass = array();
+        $simboli_length = strlen($simboli) - 1;
+        for ($i = 0; $i < $number; $i++) {
+            $n = rand(0, $simboli_length);
+            $pass[] = $simboli[$n];
+        }
+        return implode($pass);
     }
-    return implode($pass);
 }
